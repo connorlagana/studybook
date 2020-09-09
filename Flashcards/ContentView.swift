949245b0d54d0c08528ecdb8c4cf35dd73b52341
Connor Lagana <connor.lagana@gmail.com>
2020-09-09 19:38:31 -0400

    Initial Commit

---
+++ Flashcards/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Flashcards
+//
+//  Created by Connor Lagana on 9/9/20.
+//  Copyright © 2020 Connor Lagana. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
