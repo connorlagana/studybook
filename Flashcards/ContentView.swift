60a122ef3475f5e1f3d41b98ed4716c3a1c5085a
Connor Lagana <connor.lagana@gmail.com>
2020-09-09 20:10:40 -0400

    fetch core data

--- Flashcards/ContentView.swift
+++ Flashcards/ContentView.swift
@@ -7,6 +7,61 @@
 //
 
 import SwiftUI
+import CoreData
+
+private func fetchCards() {
+    //fetching core data
+    let persistentContainer = NSPersistentContainer(name: "Flashcards")
+    persistentContainer.loadPersistentStores { (storeData, err) in
+        if let err = err {
+            print("failed something: \(err)")
+        }
+        
+    }
+    
+    let context = persistentContainer.viewContext
+    
+    let fetchRequest = NSFetchRequest<Flashcard>(entityName: "Flashcard")
+    
+    do {
+        let cards = try context.fetch(fetchRequest)
+        
+        cards.forEach { (flash) in
+            print("Flashcard here", flash)
+        }
+        
+    } catch let fetchErr {
+        print("fetch error: \(fetchErr)")
+    }
+    
+}
+
+private func handleSave() {
+    print("handling save...")
+    
+    //String must match .xcdatamodeld file name
+    let persistentContainer = NSPersistentContainer(name: "Flashcards")
+    
+    persistentContainer.loadPersistentStores { (storeDescription, err) in
+        
+        if let err = err {
+            fatalError("OOPS! Loading store failed: \(err)")
+        }
+        
+        let context = persistentContainer.viewContext
+        
+        let card = NSEntityDescription.insertNewObject(forEntityName: "Flashcard", into: context)
+        
+        card.setValue("How big is the sun?", forKey: "question")
+        card.setValue("Really big!", forKey: "answer")
+        
+        do {
+            try context.save()
+        } catch let saveErr {
+            print("failed to save card: \(saveErr)")
+        }
+    }
+}
 
 struct ContentView: View {
     var body: some View {
